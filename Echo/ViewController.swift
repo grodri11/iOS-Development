c4166af83b08772b06bdfede4263a3c01f859a87
Gabriel Rodriguez <gabe@gabriels-macbook-pro.local>
2017-06-18 02:20:48 -0400

    Initial Commit

---
+++ Echo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Echo
+//
+//  Created by Gabriel Rodriguez on 6/18/17.
+//  Copyright © 2017 Gabriel Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
